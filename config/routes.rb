5043e72fa8c2d751ac74c19e5895fbf55ff7c9b7
Raquel Rodriguez <rodriguez.reads@gmail.com>
2015-03-01 11:34:36 -0600

    created links controller and index view

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,13 @@ Rails.application.routes.draw do
        root 'devise/registrations#new', as: :unauthenticated_root
      end
   end
+
+  get '/links' => 'links#index', :as => :links
+  get '/links/new' => 'links#new', :as => :new_link
+  post '/links' => 'links#create'
+  get 'links/:id/edit' => 'links#edit', :as => :edit_link 
+  patch '/links/:id' => 'links#update'
+  delete 'links/:id' => 'links#destroy' 
+  get '/links/:id' => 'links#show', :as => :link
+
 end
