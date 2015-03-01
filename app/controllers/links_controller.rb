574543291ecf95076d772f296190b807c7a49e24
Raquel Rodriguez <rodriguez.reads@gmail.com>
2015-03-01 13:03:15 -0600

    added user link associations and main link redirect  and validations

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,28 @@
 class LinksController < ApplicationController
 
   def index
+    @links = Link.all
   end
-  
+
+  def new
+    @link = Link.new
+  end
+
+  def create
+    @link = Link.new({:user_id => params[:user_id], :slug => params[:slug], :target_url => params[:target_url]})
+    if @link.save
+      flash[:success] = "Link Added."
+      redirect_to links_path
+    else
+      flash[:info] = "Something is wrong with your form"
+      render "new"
+    end
+  end
+
+  def redirect
+    @link = Link.find_by(:slug => params[:slug])
+
+    redirect to @link.target_url
+  end
+
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,13 @@
 class Link < ActiveRecord::Base
+
+  belongs_to :user
+
+  validates :slug, :presence => true
+  validates :target_url, :presence => true
+
+  def standardize_target_url!
+    self.target_url.gsub!("http://", "")
+    self.target_url.gsub!("https://", "")
+  end
+  
 end
